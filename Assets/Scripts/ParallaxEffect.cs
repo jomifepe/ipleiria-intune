c6320d54adb58f6bd28d5aa98a38d18ffc4e694d
José Pereira <jmfp-b@live.com.pt>
2020-06-17 19:03:53 +0100

    Added background

---
+++ Assets/Scripts/ParallaxEffect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxEffect : MonoBehaviour
+{
+    private float length, startPos;
+    public GameObject camera;
+    public float effectAmount;
+    
+    
+    void Start()
+    {
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+    
+    void FixedUpdate()
+    {
+        float distance = (camera.transform.position.x * effectAmount);
+        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+    }
+}
