7b8f323b598f1f0a92522c1d3053fec3a3761988
Ivo <ivo.santos@soundparticles.com>
2020-07-04 19:01:26 +0100

    Code clean-up

---
+++ Assets/Scripts/RangedEnemy.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public abstract class RangedEnemy : Enemy
+{
+    [SerializeField] private GameObject throwablePrefab;
+    [SerializeField] private float shootVelocity;
+
+    protected override void Attack()
+    {
+        GameObject throwable = Instantiate(throwablePrefab, attackPoint.position, attackPoint.rotation);
+        throwable.GetComponent<Rigidbody2D>().velocity = attackPoint.right * shootVelocity;
+        audioSource.PlayOneShot(attackAudioClip);
+    }
+}
--- Assets/Scripts/Skeleton.cs
+++ Assets/Scripts/Skeleton.cs
@@ -1,28 +1,9 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Serialization;
-using UnityEngine.UI;
-
-public class Skeleton: Enemy
+﻿
+public class Skeleton: MeleeEnemy
 {
-    [SerializeField] private Transform meleeAttackPoint;
-    [SerializeField] private LayerMask playerLayerMask;
-    
-    private readonly Collider2D[] results = new Collider2D[1];
-    
     protected override void Init()
     {
         life = maxHealth = 2f;
         sensingRange = 10f;
-    }
-    
-    protected override void Attack()
-    {
-        if (Physics2D.OverlapCircleNonAlloc(meleeAttackPoint.position, attackRange, results, playerLayerMask) == 1)
-        {
-            results[0].GetComponent<PlayerController>().TakeDamage(attackDamage);
-        }
     }
 }
