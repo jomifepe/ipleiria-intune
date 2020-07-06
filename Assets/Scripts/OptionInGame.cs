416d387f8a1df9498d26614cd61df4b3b3b74825
Tiago Paulo <2161352@my.ipleria.pt>
2020-07-07 00:55:51 +0100

    FIX: buttons and volume of sound slider

---
+++ Assets/Scripts/OptionInGame.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionInGame : MonoBehaviour
+{
+    private float soundVolume;
+    [SerializeField] private Slider slide;
+
+    private void Awake()
+    {
+        soundVolume = AudioManager.Instance.GetVolume();
+        Debug.Log("sound" + soundVolume);
+        slide.value = normalize(soundVolume);
+    }
+    
+    /*void Start()
+    {
+        slide = GetComponent<Slider>();
+    }
+
+    private void Update()
+    {
+        slide.value = soundVolume;
+    }*/
+
+    public void SliderChange(float value)
+    {
+         AudioManager.Instance.ChangeMasterVolume(denormalize(value));
+    }
+    
+    private float normalize(float value)
+    {
+        float min = -80;
+        float max = 0;
+
+        float normalized = (value - min) / (max - min);
+        Debug.Log("Normalize value: " + normalized);
+        return normalized;
+    }
+
+    private float denormalize(float value)
+    {
+        float min = -80;
+        float max = 0;
+
+        float denormalize = (value * (max - min) + min);
+        Debug.Log("Denormalize value: " + denormalize);
+        return denormalize;
+    }
+    
+}
--- Assets/Scripts/Options.cs
+++ Assets/Scripts/Options.cs
@@ -1,12 +1,33 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Options : MonoBehaviour
 {
+	[SerializeField] private Slider slide;
+	private float soundVolume;
+	
 	void Start() {
-		Time.timeScale = 1;
+		//Time.timeScale = 1;
+		soundVolume = AudioManager.Instance.GetVolume();
+		//slide = GetComponent<Slider>();
+		Debug.Log(soundVolume);
+		slide.value = normalize(soundVolume);
+	}
+
+	private void Update()
+	{
+		//Debug.Log("Sound Volume: " + soundVolume);
+		//slide.value = soundVolume;
+	}
+	
+	public void SliderChange(float value)
+	{
+		//Debug.Log(value);
+		AudioManager.Instance.ChangeMasterVolume(denormalize(value));
 	}
 
 	public void GoBack(){
@@ -17,5 +38,25 @@ public class Options : MonoBehaviour
 	public void OpenCredits()
 	{
 		StartCoroutine(GameManager.Instance.LoadNextLevelAsync(-3));
+	}
+
+	private float normalize(float value)
+	{
+		float min = -80;
+		float max = 0;
+
+		float normalized = (value - min) / (max - min);
+		Debug.Log("Normalize value: " + normalized);
+		return normalized;
+	}
+
+	private float denormalize(float value)
+	{
+		float min = -80;
+		float max = 0;
+
+		float denormalize = (value * (max - min) + min);
+		Debug.Log("Denormalize value: " + denormalize);
+		return denormalize;
 	}
 }
