518343adf196aefb036044455510c72ae07b671c
Tiago Paulo <2161352@my.ipleria.pt>
2020-07-07 21:04:02 +0100

    All funcionalities of end menu work

---
+++ Assets/Scripts/EndGame.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EndGame : MonoBehaviour
+{
+    [SerializeField] private Text coinText;
+    [SerializeField] private Text discsText;
+    private String currentLevel;
+    
+    private void Start()
+    {
+        currentLevel = GameManager.Instance.GetCurrentLevel();
+        int coins = GameManager.Instance.GetCoins();
+        coinText.text = coins.ToString();
+        
+        // FALTA os Discos
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadSceneAsync(currentLevel);
+    }
+
+    public void NextLevel()
+    {
+        GameManager.Instance.LoadNextLevel();
+    }
+}
