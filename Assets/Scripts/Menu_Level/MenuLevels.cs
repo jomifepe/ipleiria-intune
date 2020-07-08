09b0fbd48727161fed9028e68b430784091b95c7
Tiago Paulo <2161352@my.ipleria.pt>
2020-07-08 17:30:44 +0100

    Refactor on Menu Level Scripts

---
+++ Assets/Scripts/Menu_Level/MenuLevels.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class MenuLevels : MonoBehaviour
+{
+    public void LoadLevel1()
+    {
+        GameManager.Instance.LoadLevel(1);
+    }
+    
+    public void LoadLevel2()
+    {
+        GameManager.Instance.LoadLevel(2);
+    }
+    
+    public void LoadLevel3() 
+    {
+        GameManager.Instance.LoadLevel(3);
+    }
+
+    public void LoadMainMenu()
+    {
+        GameManager.Instance.LoadMainMenu();
+    }
+}
